e53dad0b717ae4a3536243ec3abcde7994a973fe
gal <tanjhu@163.com>
2020-06-26 13:39:58 +0800

    list_view和detail_view

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,3 +1,4 @@
+
 from django.urls import path
 
 from . import views
@@ -5,4 +6,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
 from catalog.models import Book, Author, BookInstance, Genre
+from django.views.generic import ListView,DetailView
+
 
 def index(request):
     """View function for home page of site."""
@@ -24,3 +25,14 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'catalog/index.html', context=context)
+
+from django.views import generic
+
+class BookListView(ListView):
+    model = Book
+    context_object_name = 'book_list'
+    queryset =  Book.objects.all()
+
+class BookDetailView(DetailView):
+    """Generic class-based detail view for a book."""
+    model = Book
